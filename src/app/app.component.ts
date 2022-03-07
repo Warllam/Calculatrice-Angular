168e3ee8f2e71b42927a4c54337d21936fda04e9
Warllam <paularthur@navaud.fr>
2022-03-07 10:48:56 +0100

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,22 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  name = 'PAN';
+  //input: string;
+  op1: string = '';
+  op2: string = '';
+  selecteur: number = 1;
+  result: string = '';
+
+  pressNum(num: String) {
+    if (this.selecteur == 1) {
+      this.op1 += num;
+    } else {
+      this.op2 += num;
+    }
+    //this.calcAnswer();
+  }
 }
